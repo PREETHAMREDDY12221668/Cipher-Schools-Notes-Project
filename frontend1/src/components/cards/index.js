57cc146ecdf8b90ed329fc3741c71de0b2f94d3d
preetham <reddypreetham2004@gmail.com>
2024-07-26 00:35:06 +0530

    chanfgaidfhlijg

--- frontend1/src/components/cards/index.js
+++
@@ -1,2 +0,0 @@
-import { ToastContainer } from "react-toastify";
-<ToastContainer />
--- frontend1/src/config/types.js
+++
@@ -1,6 +0,0 @@
-export const NOTES_DATA="notes-data";
-
-const types={
-    NOTES_DATA
-}
-export default types;
